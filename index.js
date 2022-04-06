ddefebba7b94776dd2045a8f6c2a9f41326dd1b1
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-06 10:50:05 +0300

    add arithmetic calculations

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor((Math.random() * 10)) + 1;
+
+const num3 = 9;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(19,3,17,20,12,4)
